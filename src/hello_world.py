f240ae62f99f202e68908d8b53f708e226625f05
Lê Minh Tú <lemizhtu@gmail.com>
2024-12-22 18:42:27 +0700

    Our first application hello_world

---
+++ src/hello_world.py
@@ -0,0 +1,10 @@
+from pyspark.sql import SparkSession
+
+spark = SparkSession.builder \
+    .appName("hello_world") \
+    .master("local[*]") \
+    .getOrCreate()
+
+print(spark.version)
+
+spark.stop()
