1df860b495e5c0125767a0116dbe80f433925583
Lê Minh Tú <lemizhtu@gmail.com>
2024-12-23 19:55:29 +0700

    Sorting the rows of a DataFrame

--- src/load_data.py
+++ src/load_data.py
@@ -23,16 +23,9 @@ def load_stock_data(symbol: str) -> DataFrame:
 
 
 aapl_df = load_stock_data("AAPL")
-aapl_df.show()
-
-date_plus_2 = date_add(aapl_df["date"], 2)
-date_of_string = date_plus_2.cast(StringType())
-concat_column = concat(date_of_string, lit(" Hello, World!"))
-
-aapl_df.select("date", concat_column.alias("transformed_date")).show(truncate=False)
-
-aapl_df.createOrReplaceTempView("df")
-
-spark.sql("SELECT date, concat(cast(date_add(date, 2) as string), ' Hello, World!') as transformed_date FROM df").show(truncate=False)
+aapl_df.sort("date").show()
+aapl_df.sort(aapl_df["date"].desc()).show()
+aapl_df.sort(date_add(aapl_df["date"], 2).desc()).show()
+aapl_df.sort(aapl_df["date"].desc(), aapl_df["close"].desc()).show()
 
 spark.stop()
