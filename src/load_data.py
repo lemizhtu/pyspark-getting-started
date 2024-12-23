4eccf7d8e2f5a22ded2d8de45dcd83977ba634bd
Lê Minh Tú <lemizhtu@gmail.com>
2024-12-23 19:13:22 +0700

    Using column functions

--- src/load_data.py
+++ src/load_data.py
@@ -1,5 +1,6 @@
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import col
+from pyspark.sql.types import StringType
 
 spark = SparkSession.builder \
     .appName("load_data") \
@@ -21,5 +22,8 @@ column3 = col("Close")
 df.select(column1, column2, column3).show()
 
 df.select("Date", "Close", "Open").show()
+
+date_as_string = df["Date"].cast(StringType()).alias("date")
+df.select(df["Date"], date_as_string).show()
 
 spark.stop()
